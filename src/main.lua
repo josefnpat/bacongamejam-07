2cd60fdf3369da0501c539d743e1ff2365db00f7
Josef Patoprsty <seppi@josefnpat.com>
2014-03-22 23:39:57 -0400

    adding gamestates

--- src/main.lua
+++ src/main.lua
@@ -4,65 +4,14 @@ platformlib = require "platform"
 playerlib = require "player"
 narrationlib = require "narration"
 
-platforms = {}
+GS = require "gamestate"
+
+states = {}
+states.splash = require "state_splash"
+states.menu = require "state_menu"
+states.game = require "state_game"
 
 function love.load()
-  for i = 1,5 do
-    platforms["hub"..i] = platformlib.new("assets/maps/hub"..i)
-    platforms["level"..i] = platformlib.new("assets/maps/level"..i)
-  end
-  global_platform = platforms.hub1
-  nar = narrationlib.new({
-    "Cannon to right of them,",
-    "Cannon to left of them,",
-    "Cannon in front of them",
-    "Volley'd and thunder'd;",
-    "Storm'd at with shot and shell,",
-    "Boldly they rode and well,",
-    "Into the jaws of Death,",
-    "Into the mouth of Hell",
-    "Rode the six hundred."})
-end
-
-function love.update(dt)
-  global_platform:update(dt)
-  nar:update(dt)
-end
-
-function love.draw()
-  love.graphics.push()
-  love.graphics.scale(4,4)
-  local x,y = global_platform.player.hc_obj:center()
-  local w = love.graphics.getWidth()/2/4
-  local h = love.graphics.getHeight()/2/4
-  love.graphics.translate(w-x,h-y)
-  global_platform:draw()
-  love.graphics.pop()
-  if debugmode then
-    local level = "UNKNOWN"
-    for i,v in pairs(platforms) do
-      if v==global_platform then
-        level = i
-      end
-    end
-    love.graphics.print(level,16,16)
-  end
-  nar:draw()
-end
-
-function love.keypressed(key)
-  if key == "`" then
-    debugmode = not debugmode
-  elseif debugmode and (
-    key == "1" or
-    key == "2" or
-    key == "3" or
-    key == "4" or
-    key == "5" ) then
-    if love.keyboard.isDown("lshift") then
-      global_platform = platforms["hub"..key]
-    else
-      global_platform = platforms["level"..key]
-    end
-  end
+    GS.registerEvents()
+    GS.switch(states.game)
 end
---
+++ src/state_game.lua
@@ -0,0 +1,68 @@
+game = {}
+
+platforms = {}
+
+function game:init()
+
+  for i = 1,5 do
+    platforms["hub"..i] = platformlib.new("assets/maps/hub"..i)
+    platforms["level"..i] = platformlib.new("assets/maps/level"..i)
+  end
+  global_platform = platforms.hub1
+  nar = narrationlib.new({
+    "Cannon to right of them,",
+    "Cannon to left of them,",
+    "Cannon in front of them",
+    "Volley'd and thunder'd;",
+    "Storm'd at with shot and shell,",
+    "Boldly they rode and well,",
+    "Into the jaws of Death,",
+    "Into the mouth of Hell",
+    "Rode the six hundred."})
+    
+end
+
+function game:update(dt)
+  global_platform:update(dt)
+  nar:update(dt)
+end
+
+function game:draw()
+  love.graphics.push()
+  love.graphics.scale(4,4)
+  local x,y = global_platform.player.hc_obj:center()
+  local w = love.graphics.getWidth()/2/4
+  local h = love.graphics.getHeight()/2/4
+  love.graphics.translate(w-x,h-y)
+  global_platform:draw()
+  love.graphics.pop()
+  if debugmode then
+    local level = "UNKNOWN"
+    for i,v in pairs(platforms) do
+      if v==global_platform then
+        level = i
+      end
+    end
+    love.graphics.print(level,16,16)
+  end
+  nar:draw()
+end
+
+function game:keypressed(key)
+  if key == "`" then
+    debugmode = not debugmode
+  elseif debugmode and (
+    key == "1" or
+    key == "2" or
+    key == "3" or
+    key == "4" or
+    key == "5" ) then
+    if love.keyboard.isDown("lshift") then
+      global_platform = platforms["hub"..key]
+    else
+      global_platform = platforms["level"..key]
+    end
+  end
+end
+
+return game
