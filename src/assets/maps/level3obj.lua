33c290ee126bd7b6f2f233d1244f44eec514a77c
Josef Patoprsty <seppi@josefnpat.com>
2014-03-23 15:04:57 -0400

    levlex

--- src/assets/maps/level3obj.lua
+++ src/assets/maps/level3obj.lua
@@ -1,3 +1,11 @@
 local obj = {}
 
+local door = objectsdata.makeDefaultObject( "door", "Door" )
+door:addToMap( "hub1", 24,16 )
+door.nearby = function()
+  global_platform = platforms.hub4
+  music:nextTrack()
+end
+table.insert(obj,door)
+
 return obj
--- src/assets/maps/level5obj.lua
+++ src/assets/maps/level5obj.lua
@@ -1,3 +1,10 @@
 local obj = {}
 
+local door = objectsdata.makeDefaultObject( "door", "Door" )
+door:addToMap( "hub1", 17,5 )
+door.nearby = function()
+  -- game over!
+end
+table.insert(obj,door)
+
 return obj
