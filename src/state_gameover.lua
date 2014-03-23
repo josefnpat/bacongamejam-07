bda2441903a3195aba7c924c8d09ca678268be62
Josef Patoprsty <seppi@josefnpat.com>
2014-03-23 17:58:31 -0400

    asjdkhaskdflja

--- src/state_gameover.lua
+++ src/state_gameover.lua
@@ -1,7 +1,9 @@
 local over = {}
+asdasd = love.graphics.newImage("assets/gameover.png")
 
 function over:draw()
-  love.graphics.printf("YOU ARE DEAD\nTIME:"..math.floor(start),0,300,800,"center")
+  love.graphics.draw(asdasd,0,0)
+  love.graphics.printf("YOU ARE DEAD\nTIME:"..math.floor(start),0,500,800,"center")
 end
 
 return over
